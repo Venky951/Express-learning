97ea510b85ae8de0db1dd0d703b8c8fa35ccf37f
Venky951 <malothvenky12@gmail.com>
2026-01-18 08:52:52 +0530

    added Dynamic-UI using EJS

---
+++ Dynamic-UI/app.js
@@ -0,0 +1,28 @@
+//Core Module
+const path = require("path");
+
+//External Module
+const express = require("express");
+const app = express();
+
+//Local Module
+const userroutes = require("./routes/user");
+const hostroutes = require("./routes/host");
+const rootdir = require("./util/path");
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(userroutes);
+app.use(hostroutes);
+app.use(express.static(path.join("public")));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootdir, "views", "404.html"));
+});
+const port = 8790;
+app.listen(port, () => {
+  console.log(`Server is listening on port http://localhost:${port}`);
+});
